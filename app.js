7d5e9205f50f650439b0565e0a12aa20a1592c32
gabaal <geraldbaalham@live.co.uk>
2024-01-12 10:27:28 +0000

    Create initial files

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log(`JS Working`);
+
+const nature = document.getElementById(`nature`);
+const playBtn = document.getElementById(`play`);
+const stopBtn = document.getElementById(`stop`);
+const pauseBtn = document.getElementById(`pause`);
+const skipFor = document.getElementById(`forward`);
+const skipBack = document.getElementById(`back`);
+
+playBtn.addEventListener(`click`, function () {
+  nature.play();
+});
